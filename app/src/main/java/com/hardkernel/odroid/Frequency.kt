8563ee0b6cb45969783d76b7b442451609131d3c
Luke Go <luke.go@hardkernel.com>
2018-03-15 12:00:17 +0900

    add system node accessor on frequency.

--- app/src/main/java/com/hardkernel/odroid/Frequency.kt
+++ app/src/main/java/com/hardkernel/odroid/Frequency.kt
@@ -18,42 +18,35 @@ class Frequency(private val TAG: String, private val cluster: Cluster) {
 
     val scalingCurrent: String?
         get() {
-            var freq: String? = null
-            try {
-                val fileReader = when (cluster) {
-                    CPU.Cluster.Big -> FileReader(SystemNode.bigScalingMaxFreq)
-                    CPU.Cluster.Little -> FileReader(SystemNode.littleScalingMaxFreq)
-                }
-
-                val bufferedReader = BufferedReader(fileReader)
-                freq = bufferedReader.readLine()
-                bufferedReader.close()
-                Log.e(TAG, freq)
-            } catch (e: Exception) {
-                e.printStackTrace()
+            val freq: String? = when (cluster) {
+                Cluster.Big -> getSystemValue(SystemNode.bigScalingMaxFreq)
+                Cluster.Little -> getSystemValue(SystemNode.littleScalingMaxFreq)
             }
-
+            Log.e(TAG, "Current frequency : $freq")
             return freq
         }
 
     private val scalingAvailables: String?
         get() {
-            var availableFrequencies: String? = null
-            try {
-                val fileReader = when (cluster) {
-                    CPU.Cluster.Big -> FileReader(SystemNode.bigScalingAvailableFreq)
-                    CPU.Cluster.Little -> FileReader(SystemNode.littleScalingAvailableFreq)
-                }
-
-                val bufferedReader = BufferedReader(fileReader)
-                availableFrequencies = bufferedReader.readLine()
-                bufferedReader.close()
-                Log.e(TAG, availableFrequencies)
-            } catch (e: Exception) {
-                e.printStackTrace()
+            val availableFrequencies: String? = when (cluster) {
+                Cluster.Big -> getSystemValue(SystemNode.bigScalingAvailableFreq)
+                Cluster.Little -> getSystemValue(SystemNode.littleScalingAvailableFreq)
             }
+            Log.e(TAG, "Available Frequencies : $availableFrequencies")
             return availableFrequencies
         }
+
+    private fun getSystemValue(node:String): String? {
+        return try {
+            val reader =BufferedReader(FileReader(node))
+            val value = reader.readLine()
+            reader.close()
+            value
+        } catch (e:Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
 
     fun setScalingMax(freq: String) {
         try {
